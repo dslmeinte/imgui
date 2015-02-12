c121ccc78cc73fa0e07496c76cab1b2f42296cf7
Meinte Boersma <Meinte.Boersma@mendix.com>
2015-02-12 18:18:06 +0100

    supported TrDD (==Train-Driven Development) by downloading CDN dependencies

---
+++ libimgui/download-dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl http://code.jquery.com/jquery-1.11.0.min.js > dist/jquery-1.11.0.min.js
+curl https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js > dist/bootstrap.min.js
+curl https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css > dist/bootstrap.min.css
